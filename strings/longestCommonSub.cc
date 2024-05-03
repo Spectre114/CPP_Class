2ae701a60c4e47c4f88d08d5fa93c95de50c5c46
Kartickey Aggarwal <hmtv2003@gmail.com>
2024-05-03 23:49:44 +0530

    3-May-2024

--- strings/longestCommonSub.cc
+++ strings/longestCommonSub.cc
@@ -1,31 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-pair<string, int> LCS(string str, string sub, vector<vector<int>> dp)
+pair<string, int> LCS(string str, string sub, vector<vector<int>> matrix)
 {
     int m = str.size();
     int n = sub.size();
     string subSeq;
+    int prev = 0;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
-            if (str[j - 1] == sub[i - 1])
+            if (sub[i - 1] == str[j - 1])
             {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
+                matrix[i][j] = 1 + matrix[i - 1][j - 1];
+                if ((matrix[i][j] - 1) == prev)
+                {
+                    subSeq += str[j - 1];
+                    prev = matrix[i][j];
+                }
             }
             else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]);
         }
     }
-    return {subSeq, dp[n][m]};
+    return {subSeq, matrix[n][m]};
 }
 int main()
 {
     string str, sub;
+    cout << "Enter the two strings: ";
     cin >> str >> sub;
     int m = str.size();
     int n = sub.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    pair<string, int> res = LCS(str, sub, dp);
-    cout << res.first << " " << res.second << endl;
+    vector<vector<int>> matrix(n + 1, vector<int>(m + 1, 0));
+    pair<string, int> subSeq = LCS(str, sub, matrix);
+    cout << subSeq.first << " " << subSeq.second;
 }
